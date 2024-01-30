330a18293c5ec26b0c445bc88835695e8de56780
amenybettaher <bettaherameni1919@gmail.com>
2024-01-30 16:01:27 +0100

    some updates

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
-import Navbar from '../css/Navbar.css'
 export const Sidebar = [
     {
         title: 'Home',
