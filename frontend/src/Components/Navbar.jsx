c0ba93e95953350bb2851f9dd745865302cd700c
amenybettaher <bettaherameni1919@gmail.com>
2024-01-31 16:45:40 +0100

    side bar , slider done

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,17 +1,14 @@
+// Navbar.jsx
 import React from 'react';
-import './Navbar.css';
-import SearchComponent from './SearchComponent'
+import '../css/Navbar.css';
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">Your Logo</div>
       <ul className="nav-links">
-        {/* <li onClick={() => props.switchView('Home')}>Home</li>
-        <li onClick={() => props.switchView('About')}>About</li>
-        <li onClick={() => props.switchView('Contact')}>Contact</li> */}
+        {/* Add any navigation links if needed */}
       </ul>
-      <div className='a'><SearchComponent /></div>
     </nav>
   );
 };
