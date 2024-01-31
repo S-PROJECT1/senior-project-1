e4d8ce8efd9fd9bd13fd068eee4bea00fed54f5c
amenybettaher <bettaherameni1919@gmail.com>
2024-01-31 23:02:03 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Handiwork from './Components/HandiWork.jsx';
 import HandMade from './Components/HandMade.jsx';
 import Navbar from './Components/Navbar';
 import Slider from './Components/Slider.jsx'; 
+import AboutUs from './Components/About.jsx';
 import { FaTools, FaHandPaper } from 'react-icons/fa';
 import { AiFillHome, AiFillPhone, AiFillPlusCircle } from 'react-icons/ai';
 import '../src/css/App.css';
@@ -52,6 +53,7 @@ function App() {
       <div className={`container ${menuView ? 'active' : ''}`}>
         {view === 'Handiwork' && <Handiwork  />}
         {view === 'HandMade' && <HandMade />}
+        {view === 'About' && <AboutUs />}
       </div>
     </div>
   );
