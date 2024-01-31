d106a79150e72a2f7bad61965787e39a50ee65cf
amenybettaher <bettaherameni1919@gmail.com>
2024-01-31 18:18:13 +0100

    test

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import '../css/Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Your   Logo</div>
+      <div className="logo">Your Logo</div>
       <ul className="nav-links">
         {/* Add any navigation links if needed */}
       </ul>
