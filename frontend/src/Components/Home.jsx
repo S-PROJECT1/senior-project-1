b51c4efce8ce438693c180f03ade57030d097135
abdelhakbarbouche <barbouchea0@gmail.com>
2024-01-31 11:20:36 +0100

    done search in navbar mazl css

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Navbar from './Navbar';
 import Handiwork from './HandiWork.jsx';
 import HandMade from './HandMade.jsx';
-import SearchComponent from './SearchComponent';
+
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Handiwork />
       <HandMade />
-      <SearchComponent />
     </div>
   );
 };
