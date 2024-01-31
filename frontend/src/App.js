db11ea2a3971157d295360f06066f977169a3f97
CheniIyes <cheniiyes@gmail.com>
2024-01-31 09:46:56 +0100

    slider-nearly-done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-// App.js
-
 import './App.css';
 import React, { useState } from 'react';
 import Handiwork from './Components/HandiWork.jsx';
 import HandMade from './Components/HandMade.jsx';
 import Navbar from './Components/Navbar.jsx';
+import Slider from './Components/Slider.jsx';
+import Home from './Components/Home.jsx';
+
+
 import { FaTools, FaHandPaper } from 'react-icons/fa';
 
 function App() {
   const [menuView, setMenuView] = useState(false);
-  const [view, setView] = useState('Products');
+  const [view, setView] = useState('Home');
 
   const toggleMenu = () => {
     setMenuView(!menuView);
@@ -17,12 +19,14 @@ function App() {
 
   const switchView = (option) => {
     setView(option);
-    setMenuView(false); // Close the sidebar when switching views
+    setMenuView(false);
   };
 
   return (
     <div>
+
       <Navbar />
+      
       <div className={`sidebar ${menuView ? 'active' : ''}`}>
         <div className="katha" onClick={toggleMenu}>
           ☰
@@ -33,11 +37,18 @@ function App() {
         <span className="items" onClick={() => switchView('HandMade')}>
           <FaHandPaper className="icon" /> HandMade
         </span>
+        <span className="items" onClick={() => switchView('Home')}>
+          <FaHandPaper className="icon" /> Home
+        </span>
       </div>
 
       <div className={`container ${menuView ? 'active' : ''}`}>
+      {/* <Slider /> */}
         {view === 'Handiwork' && <Handiwork />}
         {view === 'HandMade' && <HandMade />}
+        {view === 'Home' && <Home />}
+
+
       </div>
     </div>
   );
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,15 +3,19 @@ import Navbar from './Navbar';
 import Handiwork from './HandiWork.jsx';
 import HandMade from './HandMade.jsx';
 import Search from './Search'
+import Slider from './Slider.jsx';
 const Home = () => {
   return (
     <div>
-      <Navbar />
-      <Handiwork />
-      <HandMade />
-      <Search />
+      {/* <Navbar /> */}
+      {/* <Slider /> */}
+      {/* <Handiwork /> */}
+      {/* <HandMade /> */}
+      {/* <Search /> */}
+      heloo
     </div>
   );
 };
 
 export default Home;
+
