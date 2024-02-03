d9ca4f378b36d7067c135eaa5e5fe25e0f54ea8a
CheniIyes <cheniiyes@gmail.com>
2024-02-03 10:13:31 +0100

    jwt-on-da-way

--- backend/server/index.js
+++ backend/server/index.js
@@ -12,37 +12,44 @@ const route3 = require("../routes/handywork.js");
 
 app.use(cors());
 app.use(express.json());
-app.use(session({
-  secret: 'your_session_secret',
-  resave: true,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: 'your_session_secret',
+//   resave: true,
+//   saveUninitialized: true
+// }));
 
-// check if the user is logged 
-const requireLogin = (req, res, next) => {
-  if (req.session && req.session.user) {
-    return next(); // proceed to the next if user is logged in
-  } else {
-    return res.status(401).json({ message: 'Access denied. Not logged in.' });
-  }
-};
 
+
+
+
+// // check if the user is logged 
+// const requireLogin = (req, res, next) => {
+//   if (req.session && req.session.user) {
+//     return next(); // proceed to the next if user is logged in
+//   } else {
+//     return res.status(401).json({ message: 'Access denied. Not logged in.' });
+//   }
+// };
 // jwt token middleware--------------------------------------------------------
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+// const verifyToken = (req, res, next) => {
+//   const token = req.header('Authorization');
+//   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
-  jwt.verify(token, 'your_secret_key', (err, decoded) => {
-    if (err) return res.status(401).json({ message: 'Invalid token.' });
-    req.user = decoded;
-    next();
-  });
-};
+//   jwt.verify(token,process.env.JWT_SECRET,(err, decoded) => {
+//     if (err) return res.status(401).json({ message: 'Invalid token.' });
+//     req.user = decoded;
+//     next();
+//   });
+// };
 // jwt token middleware--------------------------------------------------------
 
-app.use("/user", requireLogin, route1);
-app.use("/handmade", requireLogin, route2, verifyToken);
-app.use("/handywork", requireLogin, route3, verifyToken);
+// Apply session-based authentication middleware to specific routes
+// app.use("/user", requireLogin, route1);
+// app.use("/handmade", requireLogin, verifyToken, route2);
+// app.use("/handywork", requireLogin, verifyToken, route3);
+app.use("/user", route1);
+app.use("/handmade", route2);
+app.use("/handywork",  route3);
 
 
 
@@ -50,42 +57,9 @@ app.get('/', (req, res) => {
   res.send('Server Listening');
 })
 
-
-// app.get('/login', (req, res) => {
-//   const sql = "SELECT * FROM user WHERE email=? AND password=?"
-//   db.query(sql, [data.email, data.password], (err, result) => {
-//   })
-// });
-
-
-
-
-
-
-// login: (req, res) => {
-//   model.login(req.body, (err, results) => {
-//     if (err) {
-//       return res.json("error")
-//     }
-//     if (data.length > 0) {
-//       return res.json("success")
-//     } else {
-//       return res.json("fail")
-//     }
-//   });
-// },
-
-
-//   login: (data, callback) => {
-//     const sql = "SELECT * FROM user WHERE email=? AND password=?"
-//     connection.query(sql, [data.email, data.password], (err, result) => {
-//       callback(err, result)
-//     })
-//   },
-
-module.exports = {
-  verifyToken
-};
+// module.exports = {
+//   verifyToken
+// };
 
 app.listen(PORT, () => {
   console.log(`listen on http://localhost:${PORT}`);
