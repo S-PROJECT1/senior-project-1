72f9141c17d7cc1a6b5976d619fa2fe5119a97dc
amenybettaher <bettaherameni1919@gmail.com>
2024-02-04 14:41:17 +0100

    videos

--- frontend/src/Components/HandWorkDetails.jsx
+++ frontend/src/Components/HandWorkDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 import '../css/App.css';
-import video4 from '../videos/video4.mp4'
 
 const HandWorkDetails = ({ data, onBack }) => {
-  // // Check if data is defined before accessing its properties
-  // if (!data) {
-  //   return <div>Loading...</div>; // You can display a loading message or handle the absence of data in some way
-  // }
+  // Check if data is defined before accessing its properties
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="details-container full-screen-details">
@@ -14,9 +14,7 @@ const HandWorkDetails = ({ data, onBack }) => {
       <img src={data.image} alt="Handiwork" />
       <p>{data.desc}</p>
       {data.videos ? (
-        <video controls width="100%">
-          <source src={video4}type="video/mp4" />
-        </video>
+        <YouTube videoId={data.videos} />
       ) : (
         <p>No video available</p>
       )}
@@ -26,6 +24,10 @@ const HandWorkDetails = ({ data, onBack }) => {
 };
 
 export default HandWorkDetails;
+
+
+
+
 
 
 
