71419caf6b5443987ebe6853799d5db4e8dd4efb
amenybettaher <bettaherameni1919@gmail.com>
2024-02-04 14:00:40 +0100

    css

--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -21,7 +21,8 @@ const Register = (props) => {
             seterrors(Validation(values));
             if (errors.username === "" && errors.email === "" && errors.password === "") {
                   axios.post(`http://localhost:8080/user/add`, values)
-                        .then(res => { props.switchView('Login') })
+
+                        .then(props.switchView('Login'))
                         .catch(error => console.error('Error adding user:', error)); // Log the error here
             }
       }
